0c8f7c325c83091fbb91587706fc173728e8b8f1
krishnaprasadcp <krishnaprasad61016@gmail.com>
2025-12-09 22:23:28 +0530

    Stop tracking dist folder

--- dist/assetsEntryType.js
+++
@@ -1,6 +0,0 @@
-export const CloneMethods = {
-    DeepClone: "DeepClone",
-    ShallowClone: "ShallowClone",
-    SkeletonClone: "SkeletonClone",
-};
-//# sourceMappingURL=assetsEntryType.js.map
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoadingManager } from "./LoadingManager.js";
-export { default as AssetsLoader } from "./assetsLoader.js";
-export { default as AllAssets } from "./allAssets.js";
-//# sourceMappingURL=index.js.map
